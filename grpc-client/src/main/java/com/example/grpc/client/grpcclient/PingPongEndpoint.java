49a1729c145d0c36a20f6abe56c23bd4cbd47ec1
Daqy <klaidas.predeinas1@gmail.com>
2022-04-26 15:35:44 +0100

    html file?

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import java.nio.charset.StandardCharsets;
 
 @RestController
@@ -20,10 +22,17 @@ public class PingPongEndpoint {
 	@Autowired
 	public PingPongEndpoint(GRPCClientService grpcClientService) {
 		this.grpcClientService = grpcClientService;
-	}    
+	}
+
+	@GetMapping("/")
+	public ModelAndView welcome() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("file.html");
+		return modelAndView;
+}  
 
 	// https://stackoverflow.com/questions/31393553/how-to-read-a-multipart-file-as-a-string-in-spring
-	@GetMapping("/")
+	@PostMapping("/")
 	public String multiplyMatrix(@RequestParam("matrixFile1") MultipartFile matrixFile1, @RequestParam("matrixFile2") MultipartFile matrixFile2) {
 		try {
 			String matrixAContent = new String(matrixFile1.getBytes(), StandardCharsets.UTF_8);
