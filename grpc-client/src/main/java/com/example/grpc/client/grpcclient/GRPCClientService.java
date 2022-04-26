ef33f2d98283bf48b81e9773550a7137694b0360
Daqy <klaidas.predeinas1@gmail.com>
2022-04-26 22:27:01 +0100

    testing

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -229,14 +229,15 @@ public class GRPCClientService {
 		// MatrixReply multiplyMatrix = stud1.addBlock(MatrixRequest.newBuilder().setMatrixA(matrixA).setMatrixA(matrixB));
 
 		// System.out.printn(MatrixConversion.StringToIntArray(d3.getMatrix()));
-
+		String tmp = MatrixConversion.StringToIntArrayFromServer(a3.getMatrix());
 		// String matrix = MatrixConversion.IntArrayToString(mergeblocks(
 		// 	MatrixConversion.StringToIntArrayFromServer(a3.getMatrix()),
 		// 	MatrixConversion.StringToIntArrayFromServer(b3.getMatrix()),
 		// 	MatrixConversion.StringToIntArrayFromServer(c3.getMatrix()),
 		// 	MatrixConversion.StringToIntArrayFromServer(d3.getMatrix()))
 		// );
-		return "MatrixConversion.prettify(matrix)";
+		return tmp;
+		// return MatrixConversion.prettify(matrix);
 	};
 
 	// https://qmplus.qmul.ac.uk/pluginfile.php/2561581/mod_resource/content/0/BlockMult.java
