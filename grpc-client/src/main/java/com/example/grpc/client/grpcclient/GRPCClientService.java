5fe3c9053b4b16109bc2983db8193060d5ea468e
Daqy <klaidas.predeinas1@gmail.com>
2022-04-26 22:29:25 +0100

    testing

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -229,14 +229,14 @@ public class GRPCClientService {
 		// MatrixReply multiplyMatrix = stud1.addBlock(MatrixRequest.newBuilder().setMatrixA(matrixA).setMatrixA(matrixB));
 
 		// System.out.printn(MatrixConversion.StringToIntArray(d3.getMatrix()));
-		String tmp = MatrixConversion.StringToIntArrayFromServer(a3.getMatrix());
+		int[][] tmp = MatrixConversion.StringToIntArrayFromServer(a3.getMatrix());
 		// String matrix = MatrixConversion.IntArrayToString(mergeblocks(
 		// 	MatrixConversion.StringToIntArrayFromServer(a3.getMatrix()),
 		// 	MatrixConversion.StringToIntArrayFromServer(b3.getMatrix()),
 		// 	MatrixConversion.StringToIntArrayFromServer(c3.getMatrix()),
 		// 	MatrixConversion.StringToIntArrayFromServer(d3.getMatrix()))
 		// );
-		return tmp;
+		return "tmp";
 		// return MatrixConversion.prettify(matrix);
 	};
 
