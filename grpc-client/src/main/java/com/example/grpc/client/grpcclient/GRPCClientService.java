804603db8734ce53d652e937614bb4729b539e81
Daqy <klaidas.predeinas1@gmail.com>
2022-04-26 14:38:09 +0100

    added build

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -20,12 +20,12 @@ public class GRPCClientService {
 		int[][] matrixB = MatrixConversion.StringToIntArray(matrixBContent);
 
 		//ch
-		ManagedChannel channel1 = ManagedChannelBuilder.forAddress("localhost", 8080) .usePlaintext().build();
+		ManagedChannel channel1 = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
 
 		//stud
 		MatrixServiceGrpc.MatrixServiceBlockingStub stub1 = MatrixServiceGrpc.newBlockingStub(channel1);
 
-		MatrixReply multiplyMatrix = stub1.multiplyBlock(MatrixRequest.newBuilder().setMatrixA(matrixAContent).setMatrixB(matrixBContent));
+		MatrixReply multiplyMatrix = stub1.multiplyBlock(MatrixRequest.newBuilder().setMatrixA(matrixAContent).setMatrixB(matrixBContent).build());
 		// MatrixReply multiplyMatrix = stud1.addBlock(MatrixRequest.newBuilder().setMatrixA(matrixA).setMatrixA(matrixB));
 
 		// int[][] matrix = MatrixConversion.StringToIntArray(multiplyMatrix.getMatrix());
